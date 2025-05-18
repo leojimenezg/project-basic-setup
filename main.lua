77df8cc54a9b42c1bd33919e83009af3d9dd9503
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-05-18 02:05:06 -0600

    feat: parse_args() function to get the command line option values

--- main.lua
+++ main.lua
@@ -1,15 +1,43 @@
 local Setup = {
-	valid_options = {"--name", "--rte", "--lng", "--lcs", "--dcs"},
+	valid_options = {
+		name = "",
+		rte = "",
+		lng = "",
+		lcs = "",
+		dcs = ""
+	},
+	documents = {
+		readme = "README.md",
+		license = "LICENSE",
+		ignore = ".gitignore",
+	},
 	project_name = "",
 	project_route = "",
 	project_language = "",
 	project_license = "",
 	project_documents = {},
 
-	-- Read the command line options and parse them
+	-- Read the command line options and parse them to get their values
 	parse_args = function (self)
-		for i=1, #arg do
-			print(arg[i])
+		for i = 1, #arg do
+			local option = arg[i]
+			local possible_opt = false
+			if string.sub(option, 1, 2) == "--" then
+				possible_opt = true
+			end
+			local symbol_idx = string.find(option, "=", 5, 7)
+			if possible_opt and symbol_idx then
+				local key = string.sub(option, 3, symbol_idx - 1)
+				if self.valid_options[key] then
+					self.valid_options[key] = string.sub(option, symbol_idx + 1, -1)
+				end
+			end
+		end
+	end,
+
+	show_opts_values = function (self)
+		for key, value in pairs(self.valid_options) do
+			print(key .. " = " .. value)
 		end
 	end,
 
@@ -22,6 +50,11 @@ local Setup = {
 	write_in_documents = function (self)
 		print("")
 	end,
+
+	init = function (self)
+		self.parse_args(self)
+		self.show_opts_values(self)
+	end,
 }
 
-Setup:parse_args()
+Setup:init()
