4c9002d74e689a17afac580b11672086eb52f0d0
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-05-18 12:59:36 -0600

    refactor: move functions outside Setup table

--- main.lua
+++ main.lua
@@ -1,60 +1,63 @@
 local Setup = {
-	valid_options = {
+	-- Note: Due to how options are parsed, any new option added must be a string of exactly 3 or 4 characters long.
+	options = {
 		name = "",
 		rte = "",
 		lng = "",
-		lcs = "",
-		dcs = ""
+		lic = "",
+		dcs = "",
 	},
 	documents = {
 		readme = "README.md",
 		license = "LICENSE",
 		ignore = ".gitignore",
 	},
-	project_name = "",
-	project_route = "",
-	project_language = "",
-	project_license = "",
-	project_documents = {},
-
-	-- Read the command line options and parse them to get their values
-	parse_args = function (self)
-		for i = 1, #arg do
-			local option = arg[i]
-			local possible_opt = false
-			if string.sub(option, 1, 2) == "--" then
-				possible_opt = true
-			end
-			local symbol_idx = string.find(option, "=", 5, 7)
-			if possible_opt and symbol_idx then
-				local key = string.sub(option, 3, symbol_idx - 1)
-				if self.valid_options[key] then
-					self.valid_options[key] = string.sub(option, symbol_idx + 1, -1)
-				end
-			end
-		end
-	end,
-
-	show_opts_values = function (self)
-		for key, value in pairs(self.valid_options) do
-			print(key .. " = " .. value)
-		end
-	end,
-
-	-- Create the documents according to the parsed options
-	create_base_documents = function (self)
-		print("")
-	end,
-
-	-- Modify the created documents according to the parsed options
-	write_in_documents = function (self)
-		print("")
-	end,
-
-	init = function (self)
-		self.parse_args(self)
-		self.show_opts_values(self)
-	end,
 }
 
+-- Read the command line options and parse them to get their values
+function Setup.parse_args(self)
+	for i = 1, #arg do
+		local option = arg[i]
+		local possible_opt = false
+		if string.sub(option, 1, 2) == "--" then
+			possible_opt = true
+		end
+		local symbol_idx = string.find(option, "=", 5, 7)
+		if possible_opt and symbol_idx then
+			local key = string.sub(option, 3, symbol_idx - 1)
+			if self.options[key] then
+				self.options[key] = string.sub(option, symbol_idx + 1, -1)
+			end
+		end
+	end
+end
+
+-- Validate the received values for each option
+function Setup.validate_opts_values(self)
+	print("")
+end
+
+-- Create the documents according to the parsed options
+function Setup.create_base_documents(self)
+	print("")
+end
+
+-- Modify the created documents according to the parsed options
+function Setup.write_in_documents(self)
+	print("")
+end
+
+function Setup.show_opts_values(self)
+	for key, value in pairs(self.options) do
+		print(key .. " = " .. value)
+	end
+end
+
+function Setup.init (self)
+	self.parse_args(self)
+	self.show_opts_values(self)
+end
+
+
 Setup:init()
+
